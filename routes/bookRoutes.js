349623eab86c737bfedc9ed1f1504934eb7b6e23
Kuba <romjak0804@gamil.com>
2025-03-13 21:13:41 +0100

    created endpoints: get books, get a specific book, delete a book without logic

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const router = express.Router();
+router.use(express.json())
 
+//endpoint do wszystkich książek
+router.get('/', function(req, res){
+    res.send("Wszytkie książki");
+ });
+
+ //endpoint do wybranej książki
+router.get('/:id', (req, res) => {
+    res.send("Książka o podanym id")
+});
+
+//endpoint do usuwania książek
+router.delete('/:id', (req, res) =>{
+    res.send("Usunięcie książki o podanym id")
+});
 
 module.exports = router;
