0809e4597d3d97be2738194ae58ad1d97c310eaf
Jakub Polski <jakubpolski@gmail.com>
2025-03-19 13:43:26 +0100

    Update user model to contain information about its' role
    Added an additional description for model fields

--- models/User.js
+++ models/User.js
@@ -3,18 +3,29 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        description: 'must be a string and is required'
     },
+
     passwordHash: {
         type: String,
         required: true,
+        description: 'must be a string and is required'
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        required: true,
+        description: 'must be a string and is required'
     },
     email: {
         type: String,
         required: true,
+        description: 'must be a string and is required'
     },
     validTokens: {
         type: [String],
         default: [],
+        description: 'must be an array of strings and is not required'
     },
 });
 
