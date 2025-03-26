24c733c614dcb6ebf7da4da6bf393e3d0f305bf2
Dawid Klimkiewicz <82545462+dawidklimkiewicz@users.noreply.github.com>
2025-03-26 20:28:59 +0100

    BookBox-API-5: implemented authentication

--- models/User.ts
+++ models/User.ts
@@ -11,18 +11,28 @@ const userSchema: Schema = new Schema({
     username: {
         type: String,
         required: true,
+        description: 'must be a string and is required'
     },
     passwordHash: {
         type: String,
         required: true,
+        description: 'must be a string and is required'
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        required: true,
+        description: 'must be a string and is required'
     },
     email: {
         type: String,
         required: true,
+        description: 'must be a string and is required'
     },
     validTokens: {
         type: [String],
         default: [],
+        description: 'must be an array of strings and is not required'
     },
 });
 
